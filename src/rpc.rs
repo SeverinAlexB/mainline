25a763dc87d5d8cb9d9a980d733e80e004c3a48a
Severin Buhler <sevi_buehler@hotmail.com>
2025-06-25 12:18:59 +0200

    small changes

--- src/rpc.rs
+++ src/rpc.rs
@@ -572,9 +572,9 @@ impl Rpc {
     }
 
     fn handle_response(&mut self, from: SocketAddrV4, message: Message) -> Option<(Id, Response)> {
-        tracing::info!("Received a response from {}", from);
         // If someone claims to be readonly, then let's not store anything even if they respond.
         if message.read_only {
+            tracing::info!("Received a read-only response from {}", from);
             return None;
         };
 
@@ -596,145 +596,152 @@ impl Rpc {
             return None;
         }
 
+        tracing::debug!("Received a response from {}", from);
+
         let mut should_add_node = false;
         let author_id = message.get_author_id();
         let from_version = message.version.to_owned();
 
         // Get corresponding query for message.transaction_id
-        if let Some(query) = self
+
+        let query = match self
             .iterative_queries
             .values_mut()
             .find(|query| query.inflight(message.transaction_id))
         {
-            // KrpcSocket would not give us a response from the wrong address for the transaction_id
-            should_add_node = true;
-
-            if let Some(nodes) = message.get_closer_nodes() {
-                for node in nodes {
-                    query.add_candidate(node.clone());
-                }
+            Some(query) => query,
+            None => {
+                tracing::warn!("Received a response for an unknown query from {}", from);
+                return None;
             }
+        };
 
-            if let Some((responder_id, token)) = message.get_token() {
-                query.add_responding_node(Node::new_with_token(responder_id, from, token.into()));
+        // KrpcSocket would not give us a response from the wrong address for the transaction_id
+        should_add_node = true;
+
+        if let Some(nodes) = message.get_closer_nodes() {
+            for node in nodes {
+                query.add_candidate(node.clone());
             }
+        }
 
-            if let Some(proposed_ip) = message.requester_ip {
-                query.add_address_vote(proposed_ip);
+        if let Some((responder_id, token)) = message.get_token() {
+            query.add_responding_node(Node::new_with_token(responder_id, from, token.into()));
+        }
+
+        if let Some(proposed_ip) = message.requester_ip {
+            query.add_address_vote(proposed_ip);
+        }
+
+        let target = query.target();
+
+        match message.message_type {
+            MessageType::Response(ResponseSpecific::GetPeers(GetPeersResponseArguments {
+                values,
+                ..
+            })) => {
+                let response = Response::Peers(values);
+                query.response(from, response.clone());
+
+                return Some((target, response));
             }
-
-            let target = query.target();
-
-            match message.message_type {
-                MessageType::Response(ResponseSpecific::GetPeers(GetPeersResponseArguments {
-                    values,
-                    ..
-                })) => {
-                    let response = Response::Peers(values);
+            MessageType::Response(ResponseSpecific::GetImmutable(
+                GetImmutableResponseArguments {
+                    v, responder_id, ..
+                },
+            )) => {
+                if validate_immutable(&v, query.target()) {
+                    let response = Response::Immutable(v);
                     query.response(from, response.clone());
 
                     return Some((target, response));
                 }
-                MessageType::Response(ResponseSpecific::GetImmutable(
-                    GetImmutableResponseArguments {
-                        v, responder_id, ..
-                    },
-                )) => {
-                    if validate_immutable(&v, query.target()) {
-                        let response = Response::Immutable(v);
+
+                let target = query.target();
+                debug!(
+                    ?v,
+                    ?target,
+                    ?responder_id,
+                    ?from,
+                    ?from_version,
+                    "Invalid immutable value"
+                );
+            }
+            MessageType::Response(ResponseSpecific::GetMutable(GetMutableResponseArguments {
+                v,
+                seq,
+                sig,
+                k,
+                responder_id,
+                ..
+            })) => {
+                let salt = match query.request.request_type.clone() {
+                    RequestTypeSpecific::GetValue(args) => args.salt,
+                    _ => None,
+                };
+                let target = query.target();
+
+                match MutableItem::from_dht_message(query.target(), &k, v, seq, &sig, salt) {
+                    Ok(item) => {
+                        let response = Response::Mutable(item);
                         query.response(from, response.clone());
 
                         return Some((target, response));
                     }
-
-                    let target = query.target();
-                    debug!(
-                        ?v,
-                        ?target,
-                        ?responder_id,
-                        ?from,
-                        ?from_version,
-                        "Invalid immutable value"
-                    );
-                }
-                MessageType::Response(ResponseSpecific::GetMutable(
-                    GetMutableResponseArguments {
-                        v,
-                        seq,
-                        sig,
-                        k,
-                        responder_id,
-                        ..
-                    },
-                )) => {
-                    let salt = match query.request.request_type.clone() {
-                        RequestTypeSpecific::GetValue(args) => args.salt,
-                        _ => None,
-                    };
-                    let target = query.target();
-
-                    match MutableItem::from_dht_message(query.target(), &k, v, seq, &sig, salt) {
-                        Ok(item) => {
-                            let response = Response::Mutable(item);
-                            query.response(from, response.clone());
-
-                            return Some((target, response));
-                        }
-                        Err(error) => {
-                            debug!(
-                                ?error,
-                                ?from,
-                                ?responder_id,
-                                ?from_version,
-                                "Invalid mutable record"
-                            );
-                        }
+                    Err(error) => {
+                        debug!(
+                            ?error,
+                            ?from,
+                            ?responder_id,
+                            ?from_version,
+                            "Invalid mutable record"
+                        );
                     }
                 }
-                MessageType::Response(ResponseSpecific::NoMoreRecentValue(
-                    NoMoreRecentValueResponseArguments {
-                        seq, responder_id, ..
+            }
+            MessageType::Response(ResponseSpecific::NoMoreRecentValue(
+                NoMoreRecentValueResponseArguments {
+                    seq, responder_id, ..
+                },
+            )) => {
+                debug!(
+                    target= ?query.target(),
+                    salt= ?match query.request.request_type.clone() {
+                        RequestTypeSpecific::GetValue(args) => args.salt,
+                        _ => None,
                     },
-                )) => {
-                    debug!(
-                        target= ?query.target(),
-                        salt= ?match query.request.request_type.clone() {
-                            RequestTypeSpecific::GetValue(args) => args.salt,
-                            _ => None,
-                        },
-                        ?seq,
-                        ?from,
-                        ?responder_id,
-                        ?from_version,
-                        "No more recent value"
-                    );
-                }
-                MessageType::Response(ResponseSpecific::NoValues(NoValuesResponseArguments {
-                    responder_id,
-                    ..
-                })) => {
-                    debug!(
-                        target= ?query.target(),
-                        salt= ?match query.request.request_type.clone() {
-                            RequestTypeSpecific::GetValue(args) => args.salt,
-                            _ => None,
-                        },
-                        ?from,
-                        ?responder_id,
-                        ?from_version ,
-                        "No values"
-                    );
-                }
-                MessageType::Error(error) => {
-                    debug!(?error, ?from_version, "Get query got error response");
-                }
-                // Ping response is already handled in add_node()
-                // FindNode response is already handled in query.add_candidate()
-                // Requests are handled elsewhere
-                MessageType::Response(ResponseSpecific::Ping(_))
-                | MessageType::Response(ResponseSpecific::FindNode(_))
-                | MessageType::Request(_) => {}
-            };
+                    ?seq,
+                    ?from,
+                    ?responder_id,
+                    ?from_version,
+                    "No more recent value"
+                );
+            }
+            MessageType::Response(ResponseSpecific::NoValues(NoValuesResponseArguments {
+                responder_id,
+                ..
+            })) => {
+                debug!(
+                    target= ?query.target(),
+                    salt= ?match query.request.request_type.clone() {
+                        RequestTypeSpecific::GetValue(args) => args.salt,
+                        _ => None,
+                    },
+                    ?from,
+                    ?responder_id,
+                    ?from_version ,
+                    "No values"
+                );
+            }
+            MessageType::Error(error) => {
+                debug!(?error, ?from_version, "Get query got error response");
+            }
+            // Ping response is already handled in add_node()
+            // FindNode response is already handled in query.add_candidate()
+            // Requests are handled elsewhere
+            MessageType::Response(ResponseSpecific::Ping(_))
+            | MessageType::Response(ResponseSpecific::FindNode(_))
+            | MessageType::Request(_) => {}
         };
 
         if should_add_node {
